61db0df360607e1bf79ebc8634635069910f19e0
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-02-18 18:01:59 -0300

    "Usando for no java"

---
+++ Loops/src/Loop1.java
@@ -0,0 +1,13 @@
+
+public class Loop1 {
+
+	public static void main(String[] args) {
+		// Mostre os números de 1 a 10 usando loop
+		
+		for (int i = 1; i < 11; i++) {
+		    System.out.println(i);
+		}
+
+	}
+
+}
