29bd891a536e178497dabe5aa2b9326b45ceb170
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-02-20 13:37:22 -0300

    "For java"

---
+++ Loops/src/ParesEmLoop.java
@@ -0,0 +1,21 @@
+//import java.util.ArrayList;
+//import java.util.List;
+public class ParesEmLoop {
+
+	public static void main(String[] args) {
+		// Mostre apenas os números pares entre 1 e 100.
+		
+		//List<Integer> numeros = new ArrayList<>();
+        for (int i = 2; i <= 100; i+=2) {
+        	//numeros.add(i);
+        	System.out.println(i);
+            } 
+        //System.out.println(i);
+       }
+}
+
+
+
+
+    
+
